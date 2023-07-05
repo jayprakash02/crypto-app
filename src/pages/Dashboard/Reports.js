0dbfe7d872d347432a0a545de5ed79bef18fca79
ShriramMehta <shrirammehtasrm@gmail.com>
2023-07-05 17:07:05 +0530

    reports

--- src/pages/Dashboard/Reports.js
+++ src/pages/Dashboard/Reports.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ROI from "./reports/ROI";
+import { BI } from "./reports/BI";
+import { RI } from "./reports/RI";
+import { EI } from "./reports/EI";
 
 const Reports = ({ reportState }) => {
   return (
@@ -13,56 +16,3 @@ const Reports = ({ reportState }) => {
 };
 
 export default Reports;
-
-const RI = () => {
-  return (
-    <>
-      <h1 className="text-4xl px-12 mb-8">RI Report</h1>
-
-      <div className="bg-black w-full px-12 py-4 justify-between flex ">
-        <div className="">SI No</div>
-        <div className="">Date</div>
-        <div className="">Referral User</div>
-        <div className="">Position</div>
-        <div className="">Package</div>
-        <div className="">Percentage</div>
-        <div className="">Referral Amount</div>
-      </div>
-    </>
-  );
-};
-
-const BI = () => {
-  return (
-    <>
-      <h1 className="text-4xl px-12 mb-8">Binary Referral Report</h1>
-
-      <div className="bg-black w-full px-12 py-4 justify-between flex ">
-        <div className="">SI No</div>
-        <div className="">Date</div>
-        <div className="">Left Business</div>
-        <div className="">Right Business</div>
-        <div className="">Binary</div>
-        <div className="">Percentage</div>
-        <div className="">Amount</div>
-        <div className="">Left Carry</div>
-        <div className="">Right Carry</div>
-      </div>
-    </>
-  );
-};
-
-const EI = () => {
-  return (
-    <>
-      <h1 className="text-4xl px-12 mb-8">Extra Income Report</h1>
-
-      <div className="bg-black w-full px-12 py-4 justify-between flex ">
-        <div className="">SI No</div>
-        <div className="">Achievement</div>
-        <div className="">Reward</div>
-        <div className="">Date</div>
-      </div>
-    </>
-  );
-};
--- src/pages/Dashboard/reports/BI.js
+++ src/pages/Dashboard/reports/BI.js
@@ -2,35 +2,7 @@ import axios from "axios";
 import { baseURL } from "../../../constants/Constant";
 import { toast } from "react-hot-toast";
 import React, { useState } from "react";
-
-const ROI = () => {
-  // Dummy data for the table
-
-  const dummyData = [
-    {
-      siNo: 1,
-      date: "2023-06-01",
-      package: "Package A",
-      percentage: 10,
-      amount: 1000,
-    },
-    {
-      siNo: 2,
-      date: "2023-06-02",
-      package: "Package B",
-      percentage: 15,
-      amount: 1500,
-    },
-    {
-      siNo: 3,
-      date: "2023-06-03",
-      package: "Package C",
-      percentage: 12,
-      amount: 1200,
-    },
-    // Add more rows as needed
-  ];
-
+export const BI = () => {
   React.useEffect(() => {
     const userData = JSON.parse(localStorage.getItem("user_data"));
     if (userData && userData?.token && userData?.email && userData?.userId) {
@@ -94,34 +66,21 @@ const ROI = () => {
       toast.error("Please sign in again");
     }
   }, []);
-
   return (
     <>
-      <h1 className="text-4xl px-12 mb-8">ROI Report</h1>
+      <h1 className="text-4xl px-12 mb-8">Binary Referral Report</h1>
 
-      <div className="bg-black w-full px-12 py-4 justify-between flex">
+      <div className="bg-black w-full px-12 py-4 justify-between flex ">
         <div className="">SI No</div>
         <div className="">Date</div>
-        <div className="">Package</div>
+        <div className="">Left Business</div>
+        <div className="">Right Business</div>
+        <div className="">Binary</div>
         <div className="">Percentage</div>
         <div className="">Amount</div>
+        <div className="">Left Carry</div>
+        <div className="">Right Carry</div>
       </div>
-
-      {/* Render the table rows */}
-      {dummyData.map((row, index) => (
-        <div
-          key={index}
-          className="bg-black w-full px-12 py-4 justify-between flex"
-        >
-          <div className="">{row.siNo}</div>
-          <div className="">{row.date}</div>
-          <div className="">{row.package}</div>
-          <div className="">{row.percentage}%</div>
-          <div className="">{row.amount}</div>
-        </div>
-      ))}
     </>
   );
 };
-
-export default ROI;
