66583c5b54186d8c875e24da25bfcafb5a65559f
ShriramMehta <shrirammehtasrm@gmail.com>
2023-07-16 12:20:31 +0530

    url

--- src/constants/Constant.js
+++ src/constants/Constant.js
@@ -1,2 +1,3 @@
 // export const baseURL = "http://localhost:5000";
-export const baseURL = "https://ozo-backend.onrender.com";
+// export const baseURL = "https://ozo-backend.onrender.com";
+export const baseURL = "http://162.220.24.4/5000";
